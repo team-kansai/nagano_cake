a6b17b7f0bd8c71d2c536d50b85a71df3eb37d9d
ccwi87 <umbre1111a>
2021-02-16 08:54:29 +0000

    add-admin-devise

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,12 +1,33 @@
-class Admin::SessionsController < ApplicationController
+# frozen_string_literal: true
 
-  def new
-  end
+class Admin::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
 
-  def create
-  end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-  def destroy
-  end
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+    def after_sign_in_path_for(resource)
+    admin_path
+    end
+
+    def after_sign_out_path_for(resource)
+      new_admin_session_path
+    end
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
