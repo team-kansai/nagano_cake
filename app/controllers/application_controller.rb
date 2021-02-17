1f74e5edaff1b0a7e08b21ab9e6acdd2a3bbbe21
masaki-y1117 <my1117yamamoto@gmail.com>
2021-02-17 16:20:22 +0000

    public/registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    
+  def after_sign_in_path_for(resource)
+    customers_my_page_path(resource)
+  end
+
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,9 +1,20 @@
 class Public::RegistrationsController < ApplicationController
 
   def new
+    @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to customers_my_page_path
   end
 
+  
+  private
+  def customer_params
+    params.require(:customer).permit(
+      :last_name, :first_name, :last_name_kana, :first_name_kana,
+      :email, :postcode, :address, :phone_number, :password)
+  end
 end
