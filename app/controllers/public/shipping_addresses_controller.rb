97838e31eb09d0f45dc8a8f13bd874d983bb5b34
ccwi87 <umbre1111a>
2021-02-18 09:25:22 +0000

    add-public/shipping_addresses

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,18 +1,36 @@
 class Public::ShippingAddressesController < ApplicationController
 
+  before_action :authenticate_customer!
+
   def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = ShippingAddress.all
   end
 
   def create
+    shipping_address =  ShippingAddress.new(shipping_address_params)
+    shipping_address.customer_id = current_customer.id
+    shipping_address.save
+    redirect_to shipping_addresses_path
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def update
+    shipping_address = ShippingAddress.find(params[:id])
+    shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
   end
 
   def destroy
+  end
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postcode, :address, :name)
   end
 
 end
