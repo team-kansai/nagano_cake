2131437db099259d5960d02e0c46bcf207291c04
tomokoba <tomokoba64@gmail.com>
2021-02-18 02:54:03 +0000

    Add customers-unsubscribe page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,11 @@ class Customer < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+  enum is_deleted: {"1": true, "0": false}
+
+    def active_for_authentication?
+        super && (self.is_deleted === "0")
+        # is_deleted: "0"の場合は有効会員(ログイン可能)
+    end
+
 end
