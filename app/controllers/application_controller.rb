6553c0b6ffbc63e113db7d49ecb5c64de12c88fc
masaki-y1117 <my1117yamamoto@gmail.com>
2021-02-20 04:17:28 +0000

    public/registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     customers_my_page_path
-  end
-  
+  end 
+    
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: 
