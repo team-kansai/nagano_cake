29edd8be900aeeb72466962cc7c34ff85a5e51d2
masaki-y1117 <my1117yamamoto@gmail.com>
2021-02-23 04:08:58 +0000

    add-public/registrations.public/sessions

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -1,26 +1,21 @@
 class Admin::OrderedProductsController < ApplicationController
 
-  def update
-    @order_product = OrderedProduct.find(params[:id])
-    @order_product.update(production_status: params[:ordered_product][:production_status])
-    @order = @order_product.order
-    # @orders = Order.find(params[:id])
-    # @order_products = @orders.ordered_products
-    if @order_product.production_status == "製作中"
-      @order.update(order_status: "製作中" )
-    elsif @order_product.order.order_product.where(production_status: "製作完了")
-      @order.update(order_status: "発送準備中" )
-    else
-        
-    
-    end
-    redirect_to admin_order_path(params[:order_id])
+def update
+  @order_product = OrderedProduct.find(params[:id])
+  @order_product.update(production_status: params[:ordered_product][:production_status])
+  @order = @order_product.order
+  if @order_product.production_status == "製作中"
+    @order.update(order_status: "製作中" )
+  elsif @order_product.order.ordered_products.all?{|ordered_product| ordered_product.production_status == "製作完了" }
+    @order.update(order_status: "発送準備中" )
   end
+  redirect_to admin_order_path(params[:order_id])
+end
 
   private
   def ordered_product_params
     params.require(:ordered_product).permit(:production_status)
   end
-    
+
 
 end
